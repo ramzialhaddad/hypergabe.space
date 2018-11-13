572ed61c81b09878acfe8e77c00a82d1e237d327
Gabriel FM <gabrielfm5566@gmail.com>
2018-11-13 02:54:44 +0000

    Edited the sprite for pixiTest

--- coolstuff/Pixitest/pixiTest.js
+++ coolstuff/Pixitest/pixiTest.js
@@ -7,16 +7,31 @@ document.getElementById('display').appendChild(renderer.view);
 var stage = new PIXI.Container();
 
 PIXI.loader
-    .add("../../assets/images/Jevil_battle_jumping.gif")
+    .add("jevil", "../../assets/images/Jevil_battle_jumping.gif")
     .load(setup);
 
 var jevil;
 
 function setup(){
     jevil = new PIXI.Sprite(
-        PIXI.loader.resources["../../assets/images/Jevil_battle_jumping.gif"].texture
+        PIXI.loader.resources["jevil"].texture
     );
+
     stage.addChild(jevil);
+
+    animationLoop();
+}
+
+function animationLoop(){
+    requestAnimationFrame(animationLoop);
+
+    jevil.scale.set(1.5, 1.5);
+    jevil.x = renderer.width / 2;
+    jevil.y = renderer.height / 2;
+    jevil.anchor.set(0.5, 0.5);
+
+    jevil.roation += 0.001
+
     renderer.render(stage);
 }
 
