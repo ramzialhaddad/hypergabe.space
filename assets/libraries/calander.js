190c3aafa1611e36b1eb26d9786d10f0eb92379f
Gabriel FM <gabrielfm5566@gmail.com>
2019-01-21 15:47:45 +0000

    Optimized some stuff

--- assets/libraries/calander.js
+++ assets/libraries/calander.js
@@ -1,26 +1,48 @@
+// randomizer owo
+function randomizer(array){
+	return Math.floor(Math.random() * array);
+}
+
+
+	
 // h-hewwo there 'w'
 var d = new Date();
 var month = d.getMonth();
-var day = d.getDate();
+var day =  d.getDate();
+var musicselect = null;
 
-var thingstosay = [
-	'People like you belong in the trash.',
-	'I don\'t feel like doing anything today.',
-	'You wanna play fortnite with me?',
-	'Why is a loser like you on here?',
-	'Please don\'t speak to me... or look at me.',
-	'If you like Satania then you must be a real idiot.',
-	'How do I convert skrillex.mp3 into html??',
-	'Hey I heard that HyperGabe is a pretty cool name',
-	'Please do us both a favour and get as far from me as possible',
-	'I don\'t remember asking you to be here.',
-	'I\'m waiting for .meme to become available',
-	'owo what\'s this?? *nuzzles you*',
-	'Only losers refresh the page',
-	'Go to http://kakkoi.cafe for the bootleg version of this site',
-	'mom said it\'s my turn to go on the xbox'
-];
+// page data
+var pageDefault = pageData.Default;
+var pageChristmas = pageData.Christmas;
+var pageBirthday = pageData.Birthday;
 
+// 
+if(month == 4 & day == 20){
+    console.log("Happy Birthday!!!")
+	musicselect = "birthday/"
+	todaypage = pageBirthday;	
+}else if (month == 11 & day < 27){
+	console.log("Merry Christmas!!!")
+	musicselect = "christmas/"
+	todaypage = pageChristmas;	
+} else {
+	musicselect = "default/"
+	todaypage = pageDefault;
+}
+
+
+document.getElementById("gabechan").src=todaypage.Gabe;
+
+
+var randomSong = randomizer(todaypage.Songs.length);
+document.getElementById('iframeAudio').src = "assets/songs/" + musicselect + todaypage.Songs[randomSong] + ".mp3";
+document.getElementById('SongName').innerHTML = "Song: " + todaypage.Songs[randomSong];
+	
+var randomMessage = randomizer(todaypage.Messages.length);
+document.getElementById('randomtext').innerHTML = todaypage.Messages[randomMessage];
+
+
+/*
 if (month == 11 & day < 27){
     console.log("Merry Christmas!!!")
 	console.log(month)
@@ -62,23 +84,6 @@ if (month == 11 & day < 27){
     document.getElementById("gabechan").src="assets/images/gabe1.png";
 
     //Music
-    var songstoplay = [
-	    '18 Carat Affair - Modus Operandi',
-	    'A Hat in Time - Peace and Tranquility',
-	    'A Z U R E - The Future Is Now',
-	    'Anri - Remember Summer Days',
-	    'bbrainz - cyber imagination',
-	    'Black Heron – Surgeon Stimulator (Heart Transplant)',
-	    'Corp - Just in Time',
-	    'hello meteor - Breeze Bay',
-	    'HOME - Resonance',
-	    'Jasper De Ceuster - Dusk',
-	    'Junko Ohashi - Telephone Number',
-	    'Surfing - Hit The Spot',
-	    'THOUGHTS - MIDNIGHT',
-	    'Zane Alexander - V o i d',
-	    'マクロスMACROSS 82-99 - 葛城 ミサトYEBISU (YUNG BAE EDIT)'
-    ];
 
     var god = Math.floor(Math.random() * 100) + 1;
     console.log(god);
@@ -93,11 +98,7 @@ if (month == 11 & day < 27){
         var randomMessage = Math.floor(Math.random() * thingstosay.length);
         var randomSong = Math.floor(Math.random() * songstoplay.length);
     }
-    document.getElementById('iframeAudio').src = "assets/songs/" + songstoplay[randomSong] + ".mp3";
-    document.getElementById('SongName').innerHTML = "Song: " + songstoplay[randomSong];
     
-    document.getElementById('randomtext').innerHTML = thingstosay[randomMessage];
 }
-
-
+*/
 
---
+++ assets/libraries/data/pageEvents.js
@@ -0,0 +1,83 @@
+pageData = {
+	"Default": {
+	    "Gabe":"assets/images/gabeDefault.png",
+		"Messages": [
+		    "People like you belong in the trash.",
+		    "I don't feel like doing anything today.",
+			"You wanna play fortnite with me?",
+			"Why is a loser like you on here?",
+			"Please don't speak to me... or look at me.",
+			"If you like Satania then you must be a real idiot.",
+			"How do I convert skrillex.mp3 into html??",
+			"Hey I heard that HyperGabe is a pretty cool name",
+			"Please do us both a favour and get as far from me as possible",
+			"I don't remember asking you to be here.",
+			"I'm waiting for .meme to become available",
+			"owo what's this?? *nuzzles you*",
+			"Only losers refresh the page",
+			"Go to http://kakkoi.cafe for the bootleg version of this site",
+			"mom said it's my turn to go on the xbox"
+		],
+		"Songs": [
+		  "18 Carat Affair - Modus Operandi",
+			"A Hat in Time - Peace and Tranquility",
+			"A Z U R E - The Future Is Now",
+			"Anri - Remember Summer Days",
+		   "bbrainz - cyber imagination",
+			"Black Heron – Surgeon Stimulator (Heart Transplant)",
+			"Corp - Just in Time",
+			"hello meteor - Breeze Bay",
+			"HOME - Resonance",
+			"Jasper De Ceuster - Dusk",
+			"Junko Ohashi - Telephone Number",
+			"Surfing - Hit The Spot",
+			"THOUGHTS - MIDNIGHT",
+			"Zane Alexander - V o i d",
+			"マクロスMACROSS 82-99 - 葛城 ミサトYEBISU (YUNG BAE EDIT)"
+		]
+	},
+	"Christmas": {
+	    "Gabe" :"assets/images/gabeSanta.png",
+	    "Messages": [
+		    "People like you belong in the trash.",
+			"I don't feel like doing anything today.",
+			"You wanna play fortnite with me?",
+		    "Why is a loser like you on here?",
+			"Please don't speak to me... or look at me.",
+			"If you like Satania then you must be a real idiot.",
+			"How do I convert skrillex.mp3 into html??",
+			"Hey I heard that HyperGabe is a pretty cool name",
+			"Please do us both a favour and get as far from me as possible",
+			"I don't remember asking you to be here.",
+			"I'm waiting for .meme to become available",
+			"owo what's this?? *nuzzles you*",
+			"Only losers refresh the page",
+			"Go to http://kakkoi.cafe for the bootleg version of this site",
+			"mom said it's my turn to go on the xbox"
+	],
+		"Songs": [
+		    "Bart Graft - Yuletide '88",
+	      	"bungalow - christmas time is here",
+	      	"CUSAX - CUSAXMAS",
+	      	"DANCE WITH THE DEAD - Carol of Bells",
+	      	"Holy Ghost! & Classixx - Merry Christmas Mr. Lawrence",
+	      	"iamMANOLIS - Xmas in Rio",
+	      	"NETVERK - LastクリスマスXMAS",
+	      	"Phaserland - What Does Christmas Mean To You",
+	      	"Street Cleaner - Flying Home For Christmas",
+	      	"Wham! - Last Christmas (Vaporwave Remix)"
+	]
+	},
+	"Birthday": {
+	    "Gabe" :"assets/images/gabeBirthday.png",
+	    "Messages": [
+			"Happy birthday to me.",
+			"I hope someone comes to my party",
+			"I don't remember inviting you here..."
+	],
+		"Songs": [
+		    "Playing with Delay - Heatbeat"
+	]
+	}
+}
+  
