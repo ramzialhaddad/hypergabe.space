9c1f886ad17cff0b4536c0810c6474a1ff25bdd9
Gabriel FM <gabrielfm5566@gmail.com>
2019-05-23 17:30:48 +0100

    Delete IpageEvents.ts
    Unneeded

--- assets/libraries/data/IpageEvents.ts
+++
@@ -1,6 +0,0 @@
-import {Song} from '../radio/Csong'
-export interface PageData{
-    icon:string        //Path to Gabe.png
-    messages:string[]  //Array of messages to say
-    playlist:Song[]    //Array of songs
-}
