48864fda47334c264f3e7e07340ec843cfc97730
Gabriel FM <gabrielfm5566@gmail.com>
2019-05-23 22:07:25 +0100

    Update CPage.js

--- assets/libraries/CPage.js
+++ assets/libraries/CPage.js
@@ -1,20 +1,18 @@
-define(["require", "exports", "./radio/Csong"], function (require, exports, Csong_1) {
+define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var pageEvents = /** @class */ (function () {
-        function pageEvents() {
-            this.icon = "../images/gabeDefault.png";
-            this.messages = new Array("NULL");
-            this.playlist = new Array(new Csong_1.Song("NULL", "NULL", "NULL"));
+        function pageEvents(pageData) {
+            this.pageData = pageData;
         }
         pageEvents.prototype.GetIcon = function () {
-            return this.icon;
+            return this.pageData.icon;
         };
         pageEvents.prototype.GetMessages = function () {
-            return this.messages;
+            return this.pageData.messages;
         };
         pageEvents.prototype.GetPlaylist = function () {
-            return this.playlist;
+            return this.pageData.playlist;
         };
         return pageEvents;
     }());
