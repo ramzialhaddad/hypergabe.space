660084c533c34e1fe1567df5b61b1bbb673b3e5f
Gabriel FM <gabrielfm5566@gmail.com>
2019-06-02 16:29:55 +0100

    Update Csong.ts
    Cleaned up the song class

--- src/assets/libraries/radio/Csong.ts
+++ src/assets/libraries/radio/Csong.ts
@@ -1,39 +1,42 @@
 export class Song {
-    private title!:string;          //Name of the song
-    private artist!:string;         //Name of the artist
-    private path!:string;           //Path of the audio file
-    private sound!:HTMLAudioElement //Audio element for file
+    private _title!: string;                 // Name of the song
+    private _artist!: string;                // Name of the artist
+    private _path!: string;                  // Path of the audio file
+    private _sound!: HTMLAudioElement;       // Audio element for file
+    private _playPause!: HTMLInputElement;   // Play/pause element
 
     constructor(title:string, artist:string, path:string){
-        this.title = title
-        this.artist = artist
-        this.path = path
-        this.sound = new Audio(path)
+        this._title = title;
+        this._artist = artist;
+        this._path = path;
+        this._sound = new Audio(path);
+
+        this._playPause = document.getElementById('playpause') as HTMLInputElement;
     }
 
-    GetTitle(){
-        return this.title;
+    get title(): string{
+        return this._title;
     }
-    GetArtist(){
-        return this.artist;
+    get artist(): string{
+        return this._artist;
     }
-    GetPath(){
-        return this.path;
+    get path(): string{
+        return this._path;
     }
-    GetAudio(){
-        return this.sound;
+    get audio(): HTMLAudioElement{
+        return this._sound;
     }
 
-    PlayAudio(){
-        this.sound.play();
-        let element = document.getElementById("playpause") as HTMLInputElement;
-        element.classList.remove("play");
-        element.classList.add("pause");
+    public PlayAudio(): void{
+        this._sound.play();
+        
+        this._playPause.classList.remove("play");
+        this._playPause.classList.add("pause");
     }
-    PauseAudio(){
-        this.sound.pause();
-        let element = document.getElementById("playpause") as HTMLInputElement;
-        element.classList.remove("pause");
-        element.classList.add("play");
+    PauseAudio(): void{
+        this._sound.pause();
+        
+        this._playPause.classList.remove("pause");
+        this._playPause.classList.add("play");
     }
 }
